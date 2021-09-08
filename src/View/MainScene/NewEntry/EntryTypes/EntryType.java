4dedb380c8a1768376179c143a1f08af9d06c911
JohnEcn <Giannhs.econ@hotmail.com>
2021-09-08 16:38:53 +0300

    Added EntryType superclass. The WebNewEntryController and ccNewEntryController subclasses override the getInstance and collectData methods.

---
+++ src/View/MainScene/NewEntry/EntryTypes/EntryType.java
@@ -0,0 +1,15 @@
+package View.MainScene.NewEntry.EntryTypes;
+
+import java.util.Map;
+
+public abstract class EntryType {
+
+    public static EntryType getInstance()
+    {
+       return null;
+    }
+    public Map<String,String> collectData()
+    {
+       return null;
+    }
+}
